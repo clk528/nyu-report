4e03900634588dec273f309b3619d2b9d2cfff55
clk528 <clk528@qq.com>
2020-02-16 15:49:14 +0800

    add views

---
+++ resources/views/email/questionnaire-notice-to-all.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>通知</title>
+</head>
+
+<body>
+<p>
+    Dear all，
+</p>
+<p>
+    According to Implement “Reporting-Zero” mechanism of Disease Prevention
+    Students, faculty, staff, and contractors shall report “zero” to the Workgroup by 3pm every day
+    through NYU Shanghai Internal Reporting System (https://access.shanghai.nyu.edu/healthcheck#/). We found that
+    you hadn’t finished the report, please submit the report ASAP.
+</p>
+<p>
+    "If you are not a registered student at NYU Shanghai, please ignore this message"
+</p>
+<p>
+    Thanks for your cooperation.
+</p>
+<p>
+    各位好，
+</p>
+<p>
+    根据实施疾病预防的“零报告”机制学生们，教师，员工和承包商应在每天下午3点之前向工作组报告“零”通过纽约大学上海内部报告系统（https://access.shanghai.nyu.edu/healthcheck#/）。我们发现您尚未完成报告，请尽快提交报告。
+</p>
+<p>
+    "如果您不是上海纽约大学在册学生，请忽略本邮件"
+</p>
+<p>
+    感谢您的合作。
+</p>
+</body>
+
+</html>
